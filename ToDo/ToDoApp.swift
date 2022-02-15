809a9c44fc5483a05d16b2778a43a5208efc89d7
sushil-mahto <sushilkr561998@gmail.com>
2022-02-15 17:42:32 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Sushil on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
